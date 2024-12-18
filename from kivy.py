6679d9e62e0a0d64d28d86dd7a9441d6b2a55b5d
Mobnsan <benyadineemohamed@gmail.com>
2024-12-18 22:18:09 +0100

    Add files via upload

---
+++ from kivy.py
@@ -0,0 +1,5 @@
+from kivy.app import App
+App().run()
+'''background: linear-gradient(45deg,antiquewhite 0%,rgb(235, 195, 144) 46%,rgb(201, 128, 34) 100%);
+    color: transparent;
+    -webkit-background-clip:text; '''
